89dba1f170ef46810d184cd0a4ba372c9614aa01
Phat0403 <dangtanphat9a1@gmail.com>
2024-08-25 10:37:07 +0700

    test upload image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import { Image } from 'antd';
 function App() {
-  const [count, setCount] = useState(0)
+  const [showImage, setShowImage] = useState(false);
+  const handleClick = () => {
+    setShowImage(true);
+  };
+  const url_images1 = Array.from({ length: 200 }, (_, i) => 'https://storage.cloud.google.com/nidim/keyframe/L01/L01_V001/'+(i + 1).toString().padStart(3, '0')+ ".jpg");
+  const url_images2 = Array.from({ length: 200 }, (_, i) => 'https://storage.cloud.google.com/nidim/keyframe/L01/L01_V001/'+(i + 1).toString().padStart(3, '0')+ ".jpg");
+  const url_images3 = Array.from({ length: 200 }, (_, i) => 'https://storage.cloud.google.com/nidim/keyframe/L01/L01_V001/'+(i + 1).toString().padStart(3, '0')+ ".jpg");
+  const url_images4 = Array.from({ length: 200 }, (_, i) => 'https://storage.cloud.google.com/nidim/keyframe/L01/L01_V001/'+(i + 1).toString().padStart(3, '0')+ ".jpg");
+  const url_images5 = Array.from({ length: 200 }, (_, i) => 'https://storage.cloud.google.com/nidim/keyframe/L01/L01_V001/'+(i + 1).toString().padStart(3, '0')+ ".jpg");
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {showImage === false?
+      (<button onClick={handleClick}>Show Image</button>)
+      :
+        (<div>
+          {url_images1.map((url) => (
+          <Image
+          width={200}
+          src= {url}
+        />))}
+      {url_images2.map((url) => (
+        <Image
+        width={200}
+        src= {url}
+      />))}
+      {url_images3.map((url) => (
+        <Image
+        width={200}
+        src= {url}
+      />))}
+      {url_images4.map((url) => (
+        <Image
+        width={200}
+        src= {url}
+      />))}
+      {url_images5.map((url) => (
+        <Image
+        width={200}
+        src= {url}
+      />))}
+        </div>)
+      }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/image.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from 'antd';
+const ImageOut = () => (
+  <Image
+    width={200}
+    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+  />
+);
+export default ImageOut;
