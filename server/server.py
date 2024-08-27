4ba85b6ee5bd723adfedb3b10a4a88bef44ba226
Phat0403 <dangtanphat9a1@gmail.com>
2024-08-27 21:03:57 +0700

    change be

---
+++ server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/users", methods=['GET'])
+
+def users():
+    return jsonify(
+        {
+            "users":[
+                'phat',
+                'duyen'
+            ]
+        }
+    )
+if  __name__ == "__main__":
+    app.run(debug=True, port=8080)
--- server/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const routerAPI = express.Router();
-// const { getUsersAPI } = require("../controllers/apiController");
-routerAPI.get("/", (req, res) => {
-  res.json({ fruits: ["apple", "orange", "banana"] });
-});
-
-module.exports = routerAPI;
